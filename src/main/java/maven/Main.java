5f167a9663f7d27675222dae72d0d3766b69c7ad
adipukr <dipu.kumar@50hertz.in>
2025-01-23 17:57:27 +0530

    new

--- src/main/java/maven/Main.java
+++ src/main/java/maven/Main.java
@@ -1,12 +1,9 @@
 package maven;
 
 public class Main {
-	
-	public static final String RESET = "\u001B[0m";
-	public static final String RED = "\u001B[31m";
-	
-	public static void main(final String[] args) {
-		System.out.printf("%shello_world%s",RED,RESET);
-		System.out.println("hello");
+	public static void main(final String[] args) throws Exception {
+		byte[][] goal = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 1, 2}};
+		System.out.println(goal.length);
+		System.out.println(goal[0].length);
 	}
 }
