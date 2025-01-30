f2fdf79d45ca874162e1eb9bfe694cbff043897b
adipukr <dipu.kumar@50hertz.in>
2025-01-30 18:38:39 +0530

    .

--- src/main/java/maven/Main.java
+++ src/main/java/maven/Main.java
@@ -1,9 +1,19 @@
 package maven;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	public static void main(final String[] args) throws Exception {
-		byte[][] goal = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 1, 2}};
-		System.out.println(goal.length);
-		System.out.println(goal[0].length);
+		List<Integer> ints = new ArrayList<>();
+		ints.add(100);
+		ints.add(200);
+		ints.add(300);
+		System.out.println(ints);
+		for (Integer elem: ints) {
+			System.out.println(elem);
+			ints.remove((Object)300);
+		}
+		System.out.println(ints);
 	}
 }
--- src/test/java/maven/AppTest.java
+++ src/test/java/maven/AppTest.java
@@ -1,12 +1,5 @@
 package maven;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
-
 public class AppTest {
 
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
 }
