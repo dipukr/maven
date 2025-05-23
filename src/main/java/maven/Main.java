2aa11d7b07a8b0ee0592eb85c980a3561de5bf6a
adipukr <dipu.kumar@50hertz.in>
2025-05-23 15:55:59 +0530

    chg

--- src/main/java/maven/Main.java
+++ src/main/java/maven/Main.java
@@ -1,10 +1,14 @@
 package maven;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class Main {
 	public static void main(final String[] args) throws Exception {
-		System.out.println(System.getProperty("HOME"));
+		java.util.List<Integer> ints = new ArrayList<>();
+		ints.add(100);
+		ints.addAll(null);
+		System.out.println(ints);
 		
 	}
 }
