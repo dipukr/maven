e97e94c163b898d54da0760608636559c4758b26
adipukr <dipu.kumar@50hertz.in>
2025-06-16 18:37:00 +0530

    chg

--- src/main/java/maven/Main.java
+++ src/main/java/maven/Main.java
@@ -1,14 +1,20 @@
 package maven;
 
-import java.io.File;
-import java.util.ArrayList;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class Main {
 	public static void main(final String[] args) throws Exception {
-		java.util.List<Integer> ints = new ArrayList<>();
-		ints.add(100);
-		ints.addAll(null);
-		System.out.println(ints);
-		
+		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+		Date date = df.parse("13-02-2025");
+		LocalDate ld = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		System.out.println(ld);
+		System.out.println(ld.getDayOfMonth());
+		System.out.println(ld.lengthOfMonth());
+		ld = ld.plusDays(ld.lengthOfMonth() - ld.getDayOfMonth() + 1);
+		System.out.println(ld);
 	}
 }
