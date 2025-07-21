caee3e44f904576235aeacb09181951e5d5eb3d6
adipukr <dipu.kumar@50hertz.in>
2025-07-21 18:18:48 +0530

    chg

--- src/main/java/maven/MonDB.java
+++ src/main/java/maven/MonDB.java
@@ -1,11 +1,16 @@
 package maven;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
@@ -16,9 +21,11 @@ import java.util.Set;
 import java.util.TimeZone;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.stream.Stream;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
@@ -71,7 +78,53 @@ public class MonDB {
 		}
 	}
 	
+	public static void getData2(MongoDatabase database) throws Exception {
+		List<String> lines = Files.lines(Path.of("/home/dkumar/nvkn.scada.csv")).toList();
+		List<ObjectId> ids = new ArrayList<>();
+		for (String line: lines) {
+			if (line.length()==24)
+			ids.add(new ObjectId(line));
+			else ids.add(new ObjectId(line.split("#")[0]));
+		}
+		PrintWriter wr = new PrintWriter("/home/dkumar/data.txt");
+		List<String> result = new ArrayList<>();
+		MongoCollection<Document> meterDataColl = database.getCollection("scada");
+		Bson filter = Filters.in("_id", ids);
+		System.out.println(meterDataColl.countDocuments(filter));
+		FindIterable<Document> meterData = meterDataColl.find(filter);
+		int count = 0;
+		for (Document doc: meterData) {
+			System.out.printf("Processing: %d\n", count++);
+			Document dc = (Document) doc.get("wind_speed");
+			Collection<Object> values = dc.values();
+			for (Object value: values) {
+				if (value != null && !value.toString().contains(".")) {
+					wr.println(doc.get("_id").toString());
+					break;
+				}
+			}
+		}
+		wr.flush();
+		wr.close();
+	}
+	
 	public static void meterData(MongoCollection<Document> meterDataColl, Set<String> meterNos, String from, String to) throws Exception {
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+		df.setTimeZone(TimeZone.getTimeZone("UTC"));
+		Date fromDate = df.parse(from);
+		Date toDate = df.parse(to);
+		Bson dateFilter = Filters.and(Filters.gte("forDate", fromDate),
+				Filters.lte("forDate", toDate));
+		Bson meterFilter = Filters.in("meterNo", meterNos);
+		Bson filter = Filters.and(dateFilter, meterFilter);
+		FindIterable<Document> meterData = meterDataColl.find(filter);
+		for (Document doc: meterData) {
+			System.out.println(doc.toJson());
+		}
+		System.out.println(meterNos);
+	}
+	
+	public static void meterData2(MongoCollection<Document> meterDataColl, Set<String> meterNos, String from, String to) throws Exception {
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
 		df.setTimeZone(TimeZone.getTimeZone("UTC"));
 		Date fromDate = df.parse(from);
@@ -89,13 +142,13 @@ public class MonDB {
 
 	public static void main(final String[] args) throws Exception {
 		// 2025-05-25T18:30:00.000+00:00
-		String uri = "mongodb://samast_user:Samast%401212@192.168.101.24:27017";
+		String uri = "mongodb://sneha:Sneha%401212@3.6.233.10:27017/?authSource=nvkn";
 		try (MongoClient mongoClient = MongoClients.create(uri)) {
-			MongoDatabase database = mongoClient.getDatabase("samast");
-			MongoCollection<Document> meterDetailsColl = database.getCollection("SecureMeterDetails");
-			MongoCollection<Document> meterDataColl = database.getCollection("pnPR_VEELoadSuveyData");
+			MongoDatabase database = mongoClient.getDatabase("nvkn");
+			//MongoCollection<Document> meterDetailsColl = database.getCollection("SecureMeterDetails");
+			//MongoCollection<Document> meterDataColl = database.getCollection("pnPR_VEELoadSuveyData");
 			//Set<String> meterNos = findAllMeterNos(meterDetailsColl);
-			getData(database, "PBB48440", "2025-05-15T18:30:00.000Z", "kwh_imp");
+			getData2(database);
 		}
 	}
 }
