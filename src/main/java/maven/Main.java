eee5cedc72bd35782af4b1ba8f789a0ee4a280a3
dipukr <adipukr@gmail.com>
2025-07-24 09:38:42 +0530

    chg

--- src/main/java/maven/Main.java
+++ src/main/java/maven/Main.java
@@ -9,24 +9,6 @@ import java.util.TreeSet;
 
 public class Main {
 	public static void main(final String[] args) throws Exception {
-		Path path = Path.of("/home/dkumar/RESEARCH/words");
-		Set<String> words = new TreeSet<>();
-		List<String> lines = Files.lines(path).toList();
-		for (String line: lines) {
-			boolean valid = true;
-			if (line.length() < 3) continue;
-			for (char ch: line.toCharArray()) {
-				if (!Character.isLetter(ch)) {
-					valid = false;
-					break;
-				}
-			}
-			if (valid) words.add(line);
-		}
-		FileWriter writer = new FileWriter(path.toFile());
-		for (String word: words)
-			writer.write(word+' ');
-		writer.flush();
-		writer.close();
+		
 	}
 }
