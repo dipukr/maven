199c9b56f7f37c04c3037975d727c0477ed1b637
adipukr <dipu.kumar@50hertz.in>
2025-08-18 18:40:50 +0530

    chg

--- src/main/java/maven/Main.java
+++ src/main/java/maven/Main.java
@@ -1,10 +1,7 @@
 package maven;
 
-class Main {
+public class Main {
 	public static void main(final String[] args) {
-		int[][][][][][] data = new int[10][10][10][11][1][1];
-		Console.log(data[0][10][29][39][278][29]);
-		Console.log("hello.world");
-		Console.log("%s", "hello.world");
+		
 	}
 }
