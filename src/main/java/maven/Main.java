43917706291504cecdd80a282708d60beeb989e3
dipukr <adipukr@gmail.com>
2025-10-14 00:13:36 +0530

    chg

--- src/main/java/maven/Main.java
+++ src/main/java/maven/Main.java
@@ -1,8 +1,25 @@
 package maven;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+record Data(String word, long count) {}
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("hello");
-		throw new RuntimeException();
+	public static void main(String[] args) throws Exception {
+		var words = new ArrayList<String>();
+		words.add("hello");
+		words.add("hello");
+		words.add("world");
+		int n = 1;
+		Map<String, Long> result = words.stream()
+				.collect(Collectors.groupingBy(elem-> elem, Collectors.counting()));
+		List<Data> datas = result.entrySet().stream()
+			.map(elem -> new Data(elem.getKey(), elem.getValue()))
+			.sorted(Comparator.comparing(Data::count)).toList();
+		System.out.println(datas.get(n - 1));
 	}
 }
