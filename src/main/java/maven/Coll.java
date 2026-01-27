5c51bec92d8fa67a596e7ddb38d36eadb8db8312
dipukr <adipukr@gmail.com>
2026-01-27 18:01:21 +0530

    change Coll.java

--- src/main/java/maven/Coll.java
+++ src/main/java/maven/Coll.java
@@ -1,12 +1,7 @@
 package maven;
 
-import java.util.Arrays;
-import java.util.TreeMap;
-
 public class Coll {
 	public static void main(String[] args) throws Exception {
-		String[] ss = "key||value".split("\\|");
-		for (String s: ss) System.out.println(s);
-		System.out.println(ss.length);
+		System.out.println("hello");
 	}
 }
